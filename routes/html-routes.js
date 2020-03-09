04b06e40656000764ecb70a218e70eb1e0f1a8bd
Robert Pierce <robert.pierce@digitaladditive.com>
2020-03-08 21:23:29 -0400

    updating gallery assets

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,4 +13,16 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+  app.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  app.get("/gallery", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/gallery.html"));
+  });
+  app.get("/contact", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/contact.html"));
+  });
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
