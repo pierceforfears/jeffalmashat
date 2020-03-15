d68aef9fe34e5eea539ac14bde1dd52a6648158e
Robert Pierce <robert.pierce@digitaladditive.com>
2020-03-15 15:20:05 -0400

    breaking everything out into separate pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,11 +16,17 @@ module.exports = function(app) {
   app.get("/about", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-  app.get("/gallery", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/gallery.html"));
+  app.get("/figures", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/figures.html"));
   });
-  app.get("/contact", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/contact.html"));
+  app.get("/elephants", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/elephants.html"));
+  });
+  app.get("/thesis", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/thesis.html"));
+  });
+  app.get("/thesis_show", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/thesis_show.html"));
   });
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
